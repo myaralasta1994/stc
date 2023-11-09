1b940b1aed945e2a4fe4d8d3f77982b0a5aecaee
pc <pc@DESKTOP-8HHL4FU>
2023-11-10 02:41:01 +0300

    k

--- chapter1/src/test/java/com/example/BookVelTest.java
+++ chapter1/src/test/java/com/example/BookVelTest.java
@@ -88,70 +88,7 @@ public class BookVelTest {
 		softAssert.assertAll();
 	}
 
-//	@Test
-//	public void bookAppointment() {
-//		// navigate to the web site
-//		driver.get("http://ref-bookvel.herokuapp.com/experts");
-//		// Validate page title
-//		// Assert.assertEquals(driver.getTitle(), "BookVel");
-//		driver.findElement(By.xpath("//a[@href=\"/book/1\"]")).click();
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//		// WebDriverWait wait = new WebDriverWait(driver,30);
-//		// wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(text(),'Please
-//		// fill the fields below.')]")));
-//		Select drpExpert = new Select(driver.findElement(By.xpath("//select[@id = 'experts']")));
-//		drpExpert.selectByIndex(1);
-//		// driver.findElement(By.id("username")).sendKeys("Myar Alasta");
-//		driver.findElement(By.cssSelector(
-//				"input[class='w-full block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline']"))
-//				.sendKeys("Myar Alasta");
-//		driver.findElements(By.id("datdsve")).size();
-//		driver.findElement(By.id("date")).sendKeys("02022021");
-//		Select drpDuration = new Select(driver.findElement(By.xpath("//select[@id = 'duration']")));
-//		drpDuration.selectByIndex(1);
-//		Select drpHours = new Select(driver.findElement(By.xpath("//select[@id = 'available_hours']")));
-//		drpHours.selectByIndex(1);
-//		driver.findElement(By.xpath("//button[@type='button']")).click();
-//
-//	}
-//
-//	@Test
-//	public void elementsExisting() {
-//		// navigate to the web site
-//		driver.get("http://ref-bookvel.herokuapp.com/experts");
-//		int x = driver
-//				.findElements(
-//						By.cssSelector("h1[class='sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4']"))
-//				.size();
-//		System.out.println(x);
-//
-//	}
-//
-//	@Test
-//	public void checkExpertPageButtons() {
-//		driver.get("http://ref-bookvel.herokuapp.com/experts");
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//		driver.findElement(By.xpath("//a[@href=\"/experts/1\"]")).click();
-//		driver.findElement(By.xpath("//a[@href=\"/experts\"]")).click();
-//		driver.findElement(By.xpath("//a[@href=\"/experts/2\"]")).click();
-//		driver.findElement(By.xpath("//a[@href=\"/experts\"]")).click();
-//		driver.findElement(By.xpath("//a[@href=\"/experts/3\"]")).click();
-//
-//	}
-//
-//	@Test
-//	public void checkExpertsIfExists() {
-//		driver.get("http://ref-bookvel.herokuapp.com/experts");
-//		List<WebElement> expertList = driver.findElements(By.tagName("h2"));
-//		for (WebElement webElement : expertList) {
-//			if (webElement.getText().contains("William Jordan") || webElement.getText().contains("Quasi Shawa")
-//					|| webElement.getText().contains("Shimaa Badawy")) {
-//				System.out.println(webElement.getText() + " is exist");
-//
-//			}
-//		}
-//
-//	}
+
 
 	@AfterMethod
 	public void afterMethod() {
